1283e87f7aa5f046afd2a9f60f4215fcdc0a40fd
Magnetic_dud <mail@dandandin.it>
2014-10-05 15:59:40 +0200

    Initial release

---
+++ SwitchIME/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SwitchIME
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string CName = "";
+            foreach (InputLanguage lang in InputLanguage.InstalledInputLanguages)
+            {
+                CName = lang.Culture.EnglishName.ToString();
+                CName = lang.LayoutName.ToString();
+
+                if (CName.StartsWith("Pinyinput"))
+                {
+                    InputLanguage.CurrentInputLanguage = lang;
+                }
+            }
+
+        }
+    }
+}
